1bcbdfbe8b42e4017cc39039f802750b04c5272d
SamCui0313 <1811636677@qq.com>
2022-06-27 21:52:56 +0800

    增加了getPage的输出功能

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import getHomework
+import re
 from rich.console import Console
 from rich.panel import Panel
 from rich.prompt import Prompt,Confirm
 from rich.text import Text
+from rich.table import Table
 
 gh = getHomework.GetHomework()
 console = Console()
@@ -12,16 +14,31 @@ panel = Panel(Text("输入\"1\"抓取作业\n输入\"2\"抓取答案\n输入\"3\
 workNum = 0
 pageNum = 0
 chooseNumber = "g"
+
 while not workNum == "q":
     console.print(panel, justify="center")
     workNum = Prompt.ask("请输入: ",choices=["1","2","3","q"])
     if workNum == "1":
-        while not chooseNumber.isdigit():
+        while not chooseNumber.isdigit():        
+            getParent_r = gh.getPage(pageNum)
+            table = Table()
+            table.add_column("[blue]序号", justify="right", width=12)
+            table.add_column("[blue]标题", width=40)
+            table.add_column("[blue]发布教师名称", width=13)
+            for i in range(pageNum*10, len(getParent_r['data'])+pageNum*10):
+                text = re.sub("\n", "", getParent_r['data'][i%10]['title'])
+                teacherName = getParent_r['data'][i%10]['creator_wx_name']
+                gh._id.append(getParent_r['data'][i%10]['_id'])
+                table.add_row(
+                    str(i + 1),
+                    text,
+                    teacherName
+                )
+            console = Console()
+            console.print(table,justify="center")
             if pageNum < 1:
-                gh.getPage(pageNum)
                 chooseNumber = Prompt.ask("请输入需要抓取的作业编号[blue](输入n翻下一页): ")
             else:
-                gh.getPage(pageNum)
                 chooseNumber = Prompt.ask("请输入需要抓取的作业编号[blue](输入n翻下一页)[yellow](输入u翻上一页): ")
             if chooseNumber == "n":
                 pageNum+=1
@@ -29,7 +46,7 @@ while not workNum == "q":
                 pageNum-=1
         chooseNumber = int(chooseNumber)
         console.print("输入\"1\"下载'作业'类型的图片\n输入\"2\"下载分类的图片")
-        type = Prompt.ask("请输入: ",choices=['1','2','3'])
+        type = Prompt.ask("请输入: ",choices=['1','2'])
         gh.getHomeworkURL(int(type),chooseNumber)
         isDownloading = Confirm.ask("本作业共有" + str(gh.feedback_number) + "人提交,是否开始下载？")
         if isDownloading:
@@ -43,12 +60,26 @@ while not workNum == "q":
             continue
     if workNum == "2":
         while not chooseNumber.isdigit():
+            getParent_r = gh.getPage(pageNum)
+            table = Table()
+            table.add_column("[blue]序号", justify="right", width=12)
+            table.add_column("[blue]标题", width=40)
+            table.add_column("[blue]发布教师名称", width=13)
+            for i in range(pageNum*10, len(getParent_r['data'])+pageNum*10):
+                text = re.sub("\n", "", getParent_r['data'][i%10]['title'])
+                teacherName = getParent_r['data'][i%10]['creator_wx_name']
+                gh._id.append(getParent_r['data'][i%10]['_id'])
+                table.add_row(
+                    str(i + 1),
+                    text,
+                    teacherName
+                )
+            console = Console()
+            console.print(table,justify="center")
             if pageNum<1:
-                gh.getPage(pageNum)
                 chooseNumber = Prompt.ask("请输入需要抓取的答案编号[blue](输入n翻下一页)：")
             else:
-                gh.getPage(pageNum)
-                chooseNumber = Prompt.ask("请输入需要抓取的作业编号[blue](输入n翻下一页)[yellow](输入u翻上一页): ")
+                chooseNumber = Prompt.ask("请输入需要抓取的答案编号[blue](输入n翻下一页)[yellow](输入u翻上一页): ")
             if chooseNumber == "n":
                 pageNum+=1
             if chooseNumber == "u":
